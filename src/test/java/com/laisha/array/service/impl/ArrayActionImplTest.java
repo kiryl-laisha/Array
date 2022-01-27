1c3065d28ca69286abd51fe5e963845b5ce1229c
kiryl-laisha <laishakaya@gmail.com>
2022-01-27 22:50:54 +0300

    pom.xml has been added.

---
+++ src/test/java/com/laisha/array/service/impl/ArrayActionImplTest.java
@@ -0,0 +1,18 @@
+package com.laisha.array.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayActionImplTest {
+
+    @Test
+    void findMinElement() {
+
+
+    }
+
+    @Test
+    void findMaxElement() {
+    }
+}
